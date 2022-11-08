b649ab3e49aa4335bc2a66823c3d9614e864f50c
Pooja_sisodia <poojasisodia406@gmail.com>
2022-11-09 01:04:21 +0530

    adding branch of tryAndCatch assignment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,16 +1,109 @@
-const UserModel= require("../models/userModel")
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/userModel");
+
+//   create user function
+const createUser = async function (req, res) {
+  try {
+    let data = req.body;
+    // req body is not empty
+    if (Object.keys(data).length == 0)
+      res.status(400).send("fill all required data");
+    let savedData = await userModel.create(data);
+    // successfully user created
+    res.status(201).send({ msg: savedData });
+  } catch (err) {//server side error
+    res.status(500).send({ msg: "err", error: err.message });
+  }
+};
+
+//   login user function
+const loginUser = async function (req, res) {
+  try {
+    let userName = req.body.emailId;
+    let password = req.body.password;
+    // finding user here
+    let user = await userModel.findOne({
+      emailId: userName,
+      password: password,
+    });
+    if (!user)
+      return res.status(400).send({
+        status: false,
+        msg: "username or the password is not corerct",
+      });
+    // creating jwt token for new login
+    let token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        batch: "lithium",
+        organisation: "FUnctionup",
+      },
+      "Hii this is pooja"
+    );
+    // sending response
+    res.setHeader("x-auth-token", token);
+    // successfully login
+    res.status(200).send({ status: true, data: token });
+  } catch (err) {
+    res.status(500).send({ msg: "err", error: err.message });
+  }
+};
+
+//   fetch user details for auttherise user
+const getUserData = async function (req, res) {
+  try {
+    let userId = req.params.userId;
+    // finding autherise user
+    let userDetails = await userModel.findById(userId);
+    // sended to client
+    res.status(200).send({ status: true, data: userDetails });
+  } catch (err) {
+    res.status(500).send({ msg: "err", error: err.message });
+  }
+};
+
+// user updating function
+const updateUser = async function (req, res) {
+  try {
+    let userId = req.params.userId;
+    let userData = req.body;
+    // updating user data after autherising
+    let updatedUser = await userModel.findOneAndUpdate(
+      { _id: userId },
+      userData,
+      { new: true }
+    );
+    res.status(200).send({ status: updatedUser, data: updatedUser });
+  } catch (err) {
+    res.status(500).send({ msg: "err", error: err.message });
+  }
+};
+
+const deleteUser = async function (req, res) {
+  try {
+    let userId = req.params.userId;
+
+    let deletedUser = await userModel.findOneAndUpdate(
+      { _id: userId },
+      { $set: { isDeleted: true } },
+      { new: true }
+    );
+    return res.status(200).send({ status: true, data: deletedUser });
+  } catch (err) {
+    res.status(500).send({ msg: "err", error: err.message });
+  }
+};
+    
+
+module.exports.createUser = createUser;
+module.exports.getUserData = getUserData;
+module.exports.updateUser = updateUser;
+module.exports.loginUser = loginUser;
+module.exports.deleteUser = deleteUser;
 
 
 
 
-const basicCode= async function(req, res) {
-    let tokenDataInHeaders= req.headers.token
-    console.log(tokenDataInHeaders)
-
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
-    res.send({ msg: "This is coming from controller (handler)"})
-    }
 
 
 
@@ -22,25 +115,3 @@ const basicCode= async function(req, res) {
 
 
 
-
-
-
-
-
-
-
-
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
-}
-
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
-
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
-module.exports.basicCode= basicCode
